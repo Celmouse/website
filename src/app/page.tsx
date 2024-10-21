4ab38638c79a90520a1b1aba398c919df88bd17e
Marcelo Viana <marcelofv12@hotmail.com>
2024-10-21 06:05:03 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <a className="flex items-center justify-center" href="#">
           <Image width={32} height={32} src={"/logo.svg"} alt="Logo" />
-          <span className="ml-2 text-lg font-bold">Celmouse</span>
+          <span className="ml-2 text-2xl font-bold">Celmouse</span>
         </a>
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <a className="text-sm font-medium hover:underline underline-offset-4" href="#features">
