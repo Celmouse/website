3eedc2e440d9c62dc8117cfefeee04dc55259504
Marcelo Viana <marcelofv12@hotmail.com>
2024-11-09 08:05:42 -0300

    fix unused

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Mouse, PresentationIcon, Gamepad2, Keyboard, SquarePlus, Speech } from "lucide-react"
 import Link from "next/link"
 import { DownloadButton } from "./(components)/download-button"
