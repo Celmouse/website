adb834c1c0a0a38f1bd3b07ce3fad5ca5e890eff
Marcelo Viana <marcelofv12@hotmail.com>
2024-12-15 08:58:50 -0300

    Update download-urls.tsx

--- src/lib/download-urls.tsx
+++ src/lib/download-urls.tsx
@@ -5,10 +5,10 @@ const linuxServerVersion = '1.2.0';
 // const appVersion = '1.0.2';
 
 export const downloadUrls = {
-  win: `https://celmouse-us.s3.us-east-1.amazonaws.com/Celmouse%2BSetup%2B${winServerVersion}.exe`,
+  win: `https://celmouse-us.s3.us-east-1.amazonaws.com/celmouse.exe`,
   linux: `https://celmouse-us.s3.us-east-1.amazonaws.com/Celmouse-${linuxServerVersion}.AppImage`,
-  macOS: `https://celmouse-us.s3.us-east-1.amazonaws.com/Celmouse-${macServerVersion}.dmg`,
-  macOSm1: `https://celmouse-us.s3.us-east-1.amazonaws.com/Celmouse-${macServerVersion}-arm64.dmg`,
+  macOS: `https://celmouse-us.s3.us-east-1.amazonaws.com/Celmouse.app.zip`,
+  macOSm1: `https://celmouse-us.s3.us-east-1.amazonaws.com/Celmouse.app.zip`,
   android: "https://play.google.com/store/apps/details?id=com.gyromouse.app",
   ios: "https://apps.apple.com/br/app/gyromouse/id6736866466",
 }
