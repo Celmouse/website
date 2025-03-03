f28c405200caa2dbec7074797fcadf76f6aa0421
Marcelo Viana <marcelofv12@hotmail.com>
2025-03-03 10:31:15 -0300

    Update download-urls.tsx

--- src/lib/download-urls.tsx
+++ src/lib/download-urls.tsx
@@ -1,7 +1,7 @@
 export const downloadUrls = {
-  win: `https://celmouse-us.s3.us-east-1.amazonaws.com/celmouse.exe`,
+  win: `https://github.com/Celmouse/celmouse/releases/download/v2.1/celmouse.exe`,
   // linux: `https://celmouse-us.s3.us-east-1.amazonaws.com/Celmouse-${linuxServerVersion}.AppImage`,
-  macOS: `https://jdayme2g2qtxil0r.public.blob.vercel-storage.com/Celmouse.app-bEHkwD1bH3fzYZCAnXmm020l9v8DrE.zip`,
+  macOS: `https://github.com/Celmouse/celmouse/releases/download/v2.1/Celmouse.app.zip`,
   android: "https://play.google.com/store/apps/details?id=com.gyromouse.app",
   ios: "https://apps.apple.com/br/app/gyromouse/id6736866466",
 }
