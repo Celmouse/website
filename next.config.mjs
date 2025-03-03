0c6d252b9101af152ecd6e76da2c9ffc000b81e1
Mathis Zerbib 🇫🇷 <33841749+MathisZerbib@users.noreply.github.com>
2025-03-03 16:28:37 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+
+const isProd = process.env.NODE_ENV = 'production';
 const nextConfig = {
+   basePath: isProd ? '/website' : '',
    output: 'export',
-  distDir: 'dist',
+   distDir: 'dist',
 };
 
 export default nextConfig;
